6ecbfe849957cf233a1aa7384b0fe595e718f98f
ZagoIris <59665991+ZagoIris@users.noreply.github.com>
2025-05-24 21:16:04 -0300

    Animacao de bote do colhereiro

--- Assets/Scripts/Puzzles/Colhereiro/EsticaCabeca.cs
+++ Assets/Scripts/Puzzles/Colhereiro/EsticaCabeca.cs
@@ -1,23 +1,34 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class EsticaCabeca : MonoBehaviour
 {
-    [SerializeField] private Vector2 EsticaPos;
+    [SerializeField] private Vector3 EsticaPos;
+    private readonly float MoveDuration = 0.2f;
+    private readonly float MaxVelocity = 2000f;
 
-    private Vector2 _initialPos;
+    private InputAction _attackAction;
+    private Vector2 _currentVelocity;
+    private Vector3 _initialPos;
 
     void Start()
     {
-        
+        _initialPos = transform.localPosition;
+        _attackAction = InputSystem.actions.FindAction("Attack");
     }
 
     void Update()
     {
-        
+        if (transform.localPosition != _initialPos)
+            ResetaPosicao();
+
+        if (_attackAction.triggered)
+            transform.localPosition = EsticaPos;
+
     }
 
-    private void OnMouseDown()
+    private void ResetaPosicao()
     {
-        
+        transform.localPosition = Vector2.SmoothDamp(transform.localPosition, _initialPos, ref _currentVelocity, MoveDuration - 0.1f, MaxVelocity);
     }
 }
--- Assets/Scripts/Puzzles/Colhereiro/OlhaMouse.cs
+++ Assets/Scripts/Puzzles/Colhereiro/OlhaMouse.cs
@@ -3,19 +3,10 @@ using UnityEngine.InputSystem;
 
 public class OlhaMouse : MonoBehaviour
 {
-    private Rigidbody2D Rigidbody;
-
-    private void Start()
-    {
-        Rigidbody = GetComponent<Rigidbody2D>();
-    }
-
-    // Update is called once per frame
     void Update()
     {
-
-        var lookDir = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
-        var angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
-        Rigidbody.SetRotation(angle);
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+        Vector2 direction = (mousePos - (Vector2)transform.position).normalized;
+        transform.up = direction;
     }
 }
