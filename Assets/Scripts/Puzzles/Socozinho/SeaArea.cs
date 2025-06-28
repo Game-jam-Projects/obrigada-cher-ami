6ead43e1a1e717a802088ff28676a0963765f1ab
marcelocsc <marcelocsc@MARCELO-LAPTOP>
2025-06-28 11:56:12 -0300

    socozinho ajustes

--- Assets/Scripts/Puzzles/Socozinho/SeaArea.cs
+++ Assets/Scripts/Puzzles/Socozinho/SeaArea.cs
@@ -12,6 +12,27 @@ public class SeaArea : Singleton<SeaArea>
 
     #endregion
 
+    #region Unity Methods
+
+#if UNITY_EDITOR
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = new Color(0f, 0.5f, 1f, 0.25f);
+
+        Vector2 size = _maxBounds - _minBounds;
+        Vector2 center = (_minBounds + _maxBounds) / 2;
+
+        Gizmos.DrawCube(center, size);
+
+        Gizmos.color = new Color(0f, 0.5f, 1f, 1f);
+        Gizmos.DrawWireCube(center, size);
+    }
+
+#endif
+
+    #endregion
+
     #region Public Methods
 
     public bool IsInsideSea(Vector2 position)
